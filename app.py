5b85987c36da8236883d5167d3984e160c4b9812
Naoki <naokim1004@gmail.com>
2026-08-18 12:15:33 +0900

    once push

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 def add(a, b):
   return a + b
 
-if __name__ == "__main__":
-  print(add(2, 3))
-
 def multiply(a, b):
   return a * b
+
+class Handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/plain")
+    self.end_headers()
+    result = f"add(2,3)={add(2,3)}, multiply(2,3)={multiply(2,3)}\n"
+    self.wfile.write(resuld.encode())
+
+if __name__ == "__main__":
+  server = HTTPServer(("0.0.0.0", 8000), Handler)
+  server.serve_forever()
